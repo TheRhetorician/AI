0106d7b3ee6347abad29f850d96a96b55dd0e851
Kriti <kriti.jethlia@yahoo.com>
2021-02-27 09:46:03 +0530

    Sending request to backend

--- A2/server.py
+++ A2/server.py
@@ -1,8 +1,11 @@
 import os
 from flask import Flask, render_template,url_for
 from flask import request
+from flask_cors import CORS
+import json
 
 app = Flask(__name__)
+CORS(app)
 app.config["DEBUG"]= True
 
 
@@ -14,14 +17,15 @@ def index():
     return render_template("index.html")   
 
 
-@app.route('/search' , methods=['POST'])
+@app.route('/getSolution' , methods=['POST'])
 def search():
     ''' 
-    Function to be called after query is inserted to return the results.
-    Data structure used includes dictionary and lists.
-    Renders the final ranked documents.
     '''
-    printf("Here is the response")
+    req_data = request.get_json()
+    print(req_data)
+    num = json.loads(req_data['number'])
+    print("Here is the response")
+    print(num)
     return render_template("index.html") 
     
 
