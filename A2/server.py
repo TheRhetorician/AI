377569b9bfbfbbdf176ac9c758d689054a80a847
Tanay Gupta <tanaygupta01@gmail.com>
2021-03-08 16:43:53 +0530

    Changed port and ip

--- A2/server.py
+++ A2/server.py
@@ -40,6 +40,4 @@ def search():
     
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=3000, debug=True)
-
-#  ,docs=[["Movie 1","Some year", "something else","another attribute"]]
+    app.run(debug=True)
